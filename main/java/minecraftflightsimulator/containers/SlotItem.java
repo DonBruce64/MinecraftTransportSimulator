6f050931a9f9e23dff1cbd0c6135e677daf9fbd7
don_bruce <rapscallion827@gmail.com>
2016-07-12 21:21:38 -0500

    Edited to allow for dummy items.

--- main/java/minecraftflightsimulator/containers/SlotItem.java
+++ main/java/minecraftflightsimulator/containers/SlotItem.java
@@ -1,11 +1,13 @@
 package minecraftflightsimulator.containers;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class SlotItem extends Slot{
+	public boolean enabled = true;
 	private final Item[] validItems;
 	
 	public SlotItem(IInventory inventory, int xDisplayPosition, int yDisplayPosition, int slotIndex, Item... validItems){
@@ -20,5 +22,13 @@ public class SlotItem extends Slot{
     		}
     	}
     	return false;
+    }
+    
+    public boolean canTakeStack(EntityPlayer player){
+        return enabled;
+    }
+    
+    public boolean func_111238_b(){
+        return enabled;
     }
 }
