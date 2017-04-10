cccec8fb9ce30d5cc0ffc9afa37e7637d76aaaa2
don_bruce <rapscallion827@gmail.com>
2017-04-09 19:07:32 -0500

    Made engine sound audible from further away.

--- main/java/minecrafttransportsimulator/sounds/EngineSound.java
+++ main/java/minecrafttransportsimulator/sounds/EngineSound.java
@@ -16,9 +16,6 @@ public class EngineSound extends MovingSound{
 	
 	private MTSVector playerPos = new MTSVector(0, 0, 0);
 	private MTSVector enginePos = new MTSVector(0, 0, 0);
-	private double playerLastX;
-	private double playerLastY;
-	private double playerLastZ;
 	private double soundVelocity;
 	
 	public EngineSound(ResourceLocation location, EntityEngine engine, float pitchFactor){
@@ -31,40 +28,34 @@ public class EngineSound extends MovingSound{
 		this.engine = engine;
 		this.pitchFactor = pitchFactor;
 		this.player = Minecraft.getMinecraft().thePlayer;
-		playerLastX = player.posX;
-		playerLastY = player.posY;
-		playerLastZ = player.posZ;
 	}
 	
 	@Override
 	public void update(){
 		if(engine.shouldSoundBePlaying()){
-			this.xPosF = (float) engine.posX;
-			this.yPosF = (float) engine.posY;
-			this.zPosF = (float) engine.posZ;
+			this.xPosF = (float) player.posX;
+			this.yPosF = (float) player.posY;
+			this.zPosF = (float) player.posZ;
 			playerPos.set(player.posX, player.posY, player.posZ);
 			enginePos.set(engine.posX, engine.posY, engine.posZ);
 			if(engine.parent != null){
 				if(player.ridingEntity instanceof EntitySeat){
 					if(engine.parent.equals(((EntitySeat) player.ridingEntity).parent)){
 						this.field_147663_c=(float) (engine.RPM/pitchFactor);
-					}else{
-						soundVelocity = (playerPos.distanceTo(enginePos) - playerPos.add(player.motionX, player.motionY, player.motionZ).distanceTo(enginePos.add(engine.parent.motionX, engine.parent.motionY, engine.parent.motionZ)));
-						this.field_147663_c=(float) (engine.RPM*(1+soundVelocity/10)/pitchFactor);
+						if(SFXSystem.isPlayerInsideVehicle()){
+							this.volume = 0.5F;
+						}else{
+							this.volume = 1;
+						}
+						return;
 					}
-					if(SFXSystem.isPlayerInsideVehicle()){
-						this.volume = 0.5F;
-					}else{
-						this.volume = 1;
-					}
-				}else{
-					soundVelocity = (playerPos.distanceTo(enginePos) - playerPos.add(player.motionX, player.motionY, player.motionZ).distanceTo(enginePos.add(engine.parent.motionX, engine.parent.motionY, engine.parent.motionZ)));
-					this.field_147663_c=(float) (engine.RPM*(1+soundVelocity/10)/pitchFactor);
-					this.volume = (float) (30*engine.RPM/pitchFactor/playerPos.distanceTo(enginePos));
 				}
-				playerLastX = player.posX;
-				playerLastY = player.posY;
-				playerLastZ = player.posZ;
+				soundVelocity = (playerPos.distanceTo(enginePos) - playerPos.add(player.motionX, player.motionY, player.motionZ).distanceTo(enginePos.add(engine.parent.motionX, engine.parent.motionY, engine.parent.motionZ)));
+				this.field_147663_c=(float) (engine.RPM*(1+soundVelocity/10)/pitchFactor);
+				this.volume = (float) (30*engine.RPM/pitchFactor/playerPos.distanceTo(enginePos));
+				if(SFXSystem.isPlayerInsideVehicle()){
+					this.volume *= 0.5F;
+				}
 			}
 		}else{
 			this.donePlaying=true;
