3088c18610ee7434be073de48ec1dd44138d8318
don_bruce <rapscallion827@gmail.com>
2017-04-30 19:50:58 -0500

    Updated to use new text system.

--- main/java/minecrafttransportsimulator/dataclasses/MTSDamageSources.java
+++ main/java/minecrafttransportsimulator/dataclasses/MTSDamageSources.java
@@ -2,9 +2,9 @@ package minecrafttransportsimulator.dataclasses;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.IChatComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
 
 public abstract class MTSDamageSources extends DamageSource{
 	//Source of the damage.  This will be the vehicle controller or the player who shot the gun.
@@ -16,23 +16,23 @@ public abstract class MTSDamageSources extends DamageSource{
 	}
 	
 	@Override
-	public IChatComponent func_151519_b(EntityLivingBase player){
-		EntityLivingBase recentEntity = player.func_94060_bK();
+	public ITextComponent getDeathMessage(EntityLivingBase player){
+		EntityLivingBase recentEntity = player.getLastAttacker();
 		if(recentEntity != null){//Player engaged with another player...
 			if(playerResponsible != null){//and then was killed by another player.
-				return new ChatComponentTranslation("death.attack." + this.damageType + ".player.player", 
-						new Object[] {player.func_145748_c_(), playerResponsible.func_145748_c_(), recentEntity.func_145748_c_()});
+				return new TextComponentTranslation("death.attack." + this.damageType + ".player.player", 
+						new Object[] {player.getDisplayName(), playerResponsible.getDisplayName(), recentEntity.getDisplayName()});
 			}else{//and then was killed by something.
-				return new ChatComponentTranslation("death.attack." + this.damageType + ".null.player", 
-						new Object[] {player.func_145748_c_(), recentEntity.func_145748_c_()});
+				return new TextComponentTranslation("death.attack." + this.damageType + ".null.player", 
+						new Object[] {player.getDisplayName(), recentEntity.getDisplayName()});
 			}
 		}else{//Player was minding their own business...
 			if(playerResponsible != null){//and was killed by another player.
-				return new ChatComponentTranslation("death.attack." + this.damageType + ".player.null", 
-						new Object[] {player.func_145748_c_(), playerResponsible.func_145748_c_()});
+				return new TextComponentTranslation("death.attack." + this.damageType + ".player.null", 
+						new Object[] {player.getDisplayName(), playerResponsible.getDisplayName()});
 			}else{//and then was killed by something.
-				return new ChatComponentTranslation("death.attack." + this.damageType + ".null.null", 
-						new Object[] {player.func_145748_c_()});
+				return new TextComponentTranslation("death.attack." + this.damageType + ".null.null", 
+						new Object[] {player.getDisplayName()});
 			}
 		}
 	                
