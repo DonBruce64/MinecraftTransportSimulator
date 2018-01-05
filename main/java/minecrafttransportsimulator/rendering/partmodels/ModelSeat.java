65230f31846eaa136442a280d6c0fa6d9063f29e
don_bruce <rapscallion827@gmail.com>
2018-01-04 21:03:58 -0600

    Made seats narrower and taller for a more seat-like look.

--- main/java/minecrafttransportsimulator/rendering/partmodels/ModelSeat.java
+++ main/java/minecrafttransportsimulator/rendering/partmodels/ModelSeat.java
@@ -14,18 +14,18 @@ public class ModelSeat extends ModelBase{
     	frame = new ModelRenderer(this, 0, 0);
     	frame.setTextureSize(textureWidth, textureHeight);
     	frame.setRotationPoint(0F, 8F, 0F);
-    	frame.addBox(-6, -10, -6, 12, 3, 12);
-    	frame.addBox(-8, -10, -6, 2, 6, 12);
-    	frame.addBox(6, -10, -6, 2, 6, 12);
-    	frame.addBox(-8, -10, -8, 16, 16, 2);
-    	frame.addBox(-8, -4, -6, 2, 10, 2);
-    	frame.addBox(6, -4, -6, 2, 10, 2);
+    	frame.addBox(-5, -10, -6, 10, 3, 12);
+    	frame.addBox(-7, -10, -6, 2, 6, 12);
+    	frame.addBox(5, -10, -6, 2, 6, 12);
+    	frame.addBox(-7, -10, -8, 14, 18, 2);
+    	frame.addBox(-7, -4, -6, 2, 12, 2);
+    	frame.addBox(5, -4, -6, 2, 12, 2);
     	
     	cushion = new ModelRenderer(this, 0, 0);
     	cushion.setTextureSize(textureWidth, textureHeight);
     	cushion.setRotationPoint(0F, 8F, 0F);
-    	cushion.addBox(-6, -7, -6, 12, 2, 12);
-    	cushion.addBox(-6, -5, -6, 12, 11, 2);  
+    	cushion.addBox(-5, -7, -6, 10, 2, 12);
+    	cushion.addBox(-5, -5, -6, 10, 13, 2);  
     }    
     
     public void renderFrame(){
