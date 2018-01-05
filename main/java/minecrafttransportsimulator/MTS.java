e2e83a45821de7ac8f83ac82ec860d14bb48221e
don_bruce <rapscallion827@gmail.com>
2018-01-04 22:01:46 -0600

    Moved mod metadata from code to mcmod.info file.

--- main/java/minecrafttransportsimulator/MTS.java
+++ main/java/minecrafttransportsimulator/MTS.java
@@ -2,18 +2,17 @@ package minecrafttransportsimulator;
 
 import java.io.File;
 
+import org.apache.logging.log4j.Logger;
+
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.ModMetadata;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = MTS.MODID, name = MTS.MODNAME, version = MTS.MODVER)
 public class MTS {
@@ -37,23 +36,10 @@ public class MTS {
 	public void PreInit(FMLPreInitializationEvent event){
 		MTSLog = event.getModLog();
 		proxy.preInit(event);
-		//this.initModMetadata(event);
 	}
 	
 	@EventHandler
 	public void Init(FMLInitializationEvent event){
 		proxy.init(event);
-	}
-	
-	private void initModMetadata(FMLPreInitializationEvent event){
-        ModMetadata meta = event.getModMetadata();
-        meta.name = "Minecraft Transport Simulator";
-        meta.description = "Realistic transport for Minecraft!";
-        meta.authorList.clear();
-        meta.authorList.add("don_bruce & CO.");
-        meta.logoFile = "Vingette.png";
-        meta.url = "http://minecraft.curseforge.com/projects/minecraft-transport-simulator";
-        meta.modId = this.MODID;
-        meta.version = this.MODVER;
 	}
 }
