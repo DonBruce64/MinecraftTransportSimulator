df6c0d02d456c7bc4c5d3c9045f94ff8dd634129
don_bruce <rapscallion827@gmail.com>
2018-01-07 20:55:45 -0600

    Updated for first release in a long time without depreciated warnings from the compiler.

--- main/java/minecrafttransportsimulator/MTS.java
+++ main/java/minecrafttransportsimulator/MTS.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class MTS {
 	public static final String MODID="mts";
 	public static final String MODNAME="Minecraft Transport Simulator";
-	public static final String MODVER="8.0.0-INDEV-30";
+	public static final String MODVER="8.0.0-INDEV-31";
 	public static final String assetDir = System.getProperty("user.dir") + File.separator + MTS.MODID;
 	
 	@Instance(value = MTS.MODID)
