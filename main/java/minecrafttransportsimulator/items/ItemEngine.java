a5bb13a446a3adb2241608d46d0d500c2e3296e0
don_bruce <rapscallion827@gmail.com>
2018-02-10 23:36:59 -0600

    Made automoative engine with higher max RPM more aking to real-life counterpart.

--- main/java/minecrafttransportsimulator/items/ItemEngine.java
+++ main/java/minecrafttransportsimulator/items/ItemEngine.java
@@ -71,8 +71,8 @@ public abstract class ItemEngine extends Item{
 		@Override
 		public ItemStack[] getAllPossibleStacks(){
 			ItemStack[] stacks = new ItemStack[2];
-			stacks[0] = getStackWithData(MTSRegistry.engineCarSmall, 3500, 0.5F);
-			stacks[1] = getStackWithData(MTSRegistry.engineCarSmall, 3500, 0.0F);
+			stacks[0] = getStackWithData(MTSRegistry.engineCarSmall, 7500, 0.5F);
+			stacks[1] = getStackWithData(MTSRegistry.engineCarSmall, 7500, 0.0F);
 			return stacks;
 		}
 	}
