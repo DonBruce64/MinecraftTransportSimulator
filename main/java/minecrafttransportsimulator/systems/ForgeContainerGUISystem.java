7bdb615b17fdfefccf17013bf5847ba19b229b27
don_bruce <rapscallion827@gmail.com>
2018-05-14 21:22:38 -0500

    Dummied out code for Forge GUI as this may not be needed.

--- main/java/minecrafttransportsimulator/systems/ForgeContainerGUISystem.java
+++ main/java/minecrafttransportsimulator/systems/ForgeContainerGUISystem.java
@@ -1,35 +1,31 @@
 package minecrafttransportsimulator.systems;
 
-import minecrafttransportsimulator.entities.parts.EntityCrate;
-import minecrafttransportsimulator.multipart.parts.PartCrate;
-import net.minecraft.client.gui.inventory.GuiChest;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.ContainerChest;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 
 public class ForgeContainerGUISystem implements IGuiHandler{
-
+	//TODO this will need to be present if we need to use the GUI system.
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
-		if(ID != -1){
+		/*if(ID != -1){
 			Entity entity = world.getEntityByID(ID);
 			if(entity instanceof EntityCrate){
 				return new ContainerChest(player.inventory, ((PartCrate) entity).crateInventory, player);
 			}
-		}
+		}*/
 		return null;
 	}
 
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
+		/*
 		if(ID != -1){
 			Entity entity = world.getEntityByID(ID);
 			if(entity instanceof EntityCrate){
 				return new GuiChest(player.inventory, (EntityCrate) entity);
 			}
-		}
+		}*/
 		return null;
 	}
 }
