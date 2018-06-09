baa3f2a4b38376fdde87caa02106d304fc448d04
don_bruce <rapscallion827@gmail.com>
2018-06-08 23:18:46 -0500

    Added disabling of blend functions to code to prevent 'leakage'.

--- main/java/minecrafttransportsimulator/rendering/blockrenders/RenderDecor.java
+++ main/java/minecrafttransportsimulator/rendering/blockrenders/RenderDecor.java
@@ -109,7 +109,7 @@ public class RenderDecor extends TileEntitySpecialRenderer<TileEntityDecor6AxisO
 			GL11.glTexCoord2f(0, 0);
 			GL11.glVertex3f(-flareSize/2F, lightYPos + lightSize/2F + flareSize/2F, 0);
 			GL11.glEnd();
-			//GL11.glDisable(GL11.GL_BLEND);
+			GL11.glDisable(GL11.GL_BLEND);
 			
 			GL11.glColor3f(1, 1, 1);
 			GL11.glDisable(GL11.GL_NORMALIZE);
