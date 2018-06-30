c0e6339000f7b0af4cab652cfe164bba2587a756
don_bruce <rapscallion827@gmail.com>
2018-06-30 17:27:18 -0500

    Added comment and removed old method.

--- main/java/minecrafttransportsimulator/rendering/RenderInstrumentsAircraft.java
+++ main/java/minecrafttransportsimulator/rendering/RenderInstrumentsAircraft.java
@@ -6,17 +6,7 @@ import net.minecraft.client.Minecraft;
 
 public final class RenderInstrumentsAircraft extends RenderInstruments{
 	private static StallSound stallBuzzer;
-
-	private static void drawGaugeBase(boolean lightsOn){
-		if(lightsOn){
-			Minecraft.getMinecraft().entityRenderer.enableLightmap();
-	    	renderSquareUV(60, 60, 0, 0.75F, 1F, 0.25F, 0F);
-	    	Minecraft.getMinecraft().entityRenderer.disableLightmap();
-		}else{
-			renderSquareUV(60, 60, 0, 0.75F, 1F, 0.25F, 0F);
-		}
-    }
-	
+	//TODO make this part of jets when we get them.
 	private static void drawLiftReserveIndicator(EntityMultipartF_Plane plane, boolean lightsOn){
         //This indicator has a special function of sounding a buzzer if the track angle is too high (stall).
         if(plane.trackAngle<=-17){
