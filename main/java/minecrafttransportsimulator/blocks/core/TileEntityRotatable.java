30ac6439ed0d97359d4e2a5c83631648f043c217
don_bruce <rapscallion827@gmail.com>
2018-09-19 22:01:55 -0500

    Fixed rotation for fuel pumps being out of bounds and crashing when updating from V10 to V11.

--- main/java/minecrafttransportsimulator/blocks/core/TileEntityRotatable.java
+++ main/java/minecrafttransportsimulator/blocks/core/TileEntityRotatable.java
@@ -4,6 +4,7 @@ import minecrafttransportsimulator.MTS;
 import minecrafttransportsimulator.packets.tileentities.PacketTileEntityClientServerHandshake;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 
 public class TileEntityRotatable extends TileEntity{
 	public byte rotation;
@@ -20,6 +21,10 @@ public class TileEntityRotatable extends TileEntity{
     public void readFromNBT(NBTTagCompound tagCompound){
         super.readFromNBT(tagCompound);
         this.rotation = tagCompound.getByte("rotation");
+        //We need to check here to make sure we don't get invalid indexes from pre-V11 fuel pumps.
+        if(rotation >= EnumFacing.VALUES.length || rotation <= EnumFacing.UP.getIndex()){
+        	rotation = (byte) EnumFacing.SOUTH.getIndex();
+        }
     }
     
 	@Override
