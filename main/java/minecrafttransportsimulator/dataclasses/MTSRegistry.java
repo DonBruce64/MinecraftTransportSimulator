ca09d1380ad1b91a2ed8d699973eeba646e04bf9
DonBruce64 <rapscallion827@gmail.com>
2019-01-13 19:39:55 -0600

    Updated wheel workbench to use new part names.

--- main/java/minecrafttransportsimulator/dataclasses/MTSRegistry.java
+++ main/java/minecrafttransportsimulator/dataclasses/MTSRegistry.java
@@ -126,7 +126,7 @@ public final class MTSRegistry{
 	public static final Item itemBlockPropellerBench = new ItemBlock(propellerBench);
 	public static final Block engineBench = new BlockPartBench("engine_aircraft", "engine_car");
 	public static final Item itemBlockEngineBench = new ItemBlock(engineBench);
-	public static final Block wheelBench = new BlockPartBench("ground_device");
+	public static final Block wheelBench = new BlockPartBench("pontoon", "skid", "tread", "wheel");
 	public static final Item itemBlockWheelBench = new ItemBlock(wheelBench);
 	public static final Block seatBench = new BlockPartBench("seat", "crate");
 	public static final Item itemBlockSeatBench = new ItemBlock(seatBench);
