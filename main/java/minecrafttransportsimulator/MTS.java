09b629c356df0ff1673b34c49a8972b22f08b5b2
DonBruce64 <rapscallion827@gmail.com>
2019-05-05 22:06:35 -0500

    Released V14.2.0.

--- main/java/minecrafttransportsimulator/MTS.java
+++ main/java/minecrafttransportsimulator/MTS.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class MTS {
 	public static final String MODID="mts";
 	public static final String MODNAME="Minecraft Transport Simulator";
-	public static final String MODVER="14.1.0";
+	public static final String MODVER="14.2.0";
 	
 	@Instance(value = MTS.MODID)
 	public static MTS instance;
