4f521fd77fed1b62bcb06cba3409961cfdc4c9c6
DonBruce64 <rapscallion827@gmail.com>
2019-10-12 14:34:38 -0500

    Start of porting all MC-specific GUI code to interface classes.

--- main/java/minecrafttransportsimulator/guis/GUIPackMissing.java
+++ main/java/minecrafttransportsimulator/guis/GUIPackMissing.java
@@ -4,73 +4,66 @@ import java.awt.Color;
 
 import org.lwjgl.opengl.GL11;
 
-import minecrafttransportsimulator.MTS;
-import net.minecraft.client.gui.GuiScreen;
+import minecrafttransportsimulator.mcinterface.MTSFontRenderer;
+import minecrafttransportsimulator.mcinterface.MTSGui;
+import minecrafttransportsimulator.mcinterface.MTSMetronome;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.util.ResourceLocation;
 
-public class GUIPackMissing extends GuiScreen{
-	private static final ResourceLocation background = new ResourceLocation(MTS.MODID, "textures/guis/wide_blank.png");
+public class GUIPackMissing extends MTSGui{
+	private int guiTextureLeft;
+	private int guiTextureTop;
 	
-	private int guiLeft;
-	private int guiTop;
-	
-	@Override 
-	public void initGui(){
-		super.initGui();
-		guiLeft = (this.width - 256)/2;
-		guiTop = (this.height - 192)/2;
+	@Override
+	public void handleInit(int width, int height){
+		guiTextureLeft = (width - 256)/2;
+		guiTextureTop = (height - 192)/2;
 	}
 	
 	@Override
-    public void drawScreen(int mouseX, int mouseY, float renderPartialTicks){
-		super.drawScreen(mouseX, mouseY, renderPartialTicks);
+	public void handleDraw(int mouseX, int mouseY){
 		GL11.glColor3f(1, 1, 1);
-		this.drawDefaultBackground();
-		mc.getTextureManager().bindTexture(background);
-		drawTexturedModalRect(guiLeft, guiTop, 0, 0, 256, 192);
+		renderBackground();
+		bindTexture(GUI_STANDARD_TEXTURE);
+		renderTexturedQuad(guiTextureLeft, guiTextureTop, 0, 0, 256, 192, 256, 192);
+
 		
-		if(mc.world.getTotalWorldTime()%40 > 20){
+		if(MTSMetronome.isOnHalf(40)){
 			GL11.glPushMatrix();
-			GL11.glTranslatef(guiLeft + 90 - fontRenderer.getStringWidth(I18n.format("gui.packmissing.title"))/2F, guiTop + 10, 0);
+			GL11.glTranslatef(guiTextureLeft + 90 - MTSFontRenderer.getTextWidth(I18n.format("gui.packmissing.title"))/2F, guiTextureTop + 10, 0);
 			GL11.glScalef(3, 3, 3);
-			fontRenderer.drawString(I18n.format("gui.packmissing.title"), 0, 0, Color.RED.getRGB());
+			MTSFontRenderer.drawText(I18n.format("gui.packmissing.title"), 0, 0, Color.RED, false, false);
 			GL11.glPopMatrix();
 		}
 		
 		GL11.glPushMatrix();
-		GL11.glTranslatef(guiLeft + 10, guiTop + 40, 0);
+		GL11.glTranslatef(guiTextureLeft + 10, guiTextureTop + 40, 0);
 		GL11.glScalef(0.75F, 0.75F, 0.75F);
-		fontRenderer.drawSplitString(I18n.format("gui.packmissing.reason"),0, 0, 320, Color.BLACK.getRGB());
+		MTSFontRenderer.drawEssay(I18n.format("gui.packmissing.reason"), 0, 0, Color.BLACK, 320);
+		
 		GL11.glPopMatrix();
 		
 		GL11.glPushMatrix();
-		GL11.glTranslatef(guiLeft + 10, guiTop + 65, 0);
+		GL11.glTranslatef(guiTextureLeft + 10, guiTextureTop + 65, 0);
 		GL11.glScalef(0.75F, 0.75F, 0.75F);
-		fontRenderer.drawSplitString(I18n.format("gui.packmissing.nomod"),0, 0, 320, Color.BLACK.getRGB());
+		MTSFontRenderer.drawEssay(I18n.format("gui.packmissing.nomod"), 0, 0, Color.BLACK, 320);
 		GL11.glPopMatrix();
 		
 		GL11.glPushMatrix();
-		GL11.glTranslatef(guiLeft + 10, guiTop + 90, 0);
+		GL11.glTranslatef(guiTextureLeft + 10, guiTextureTop + 90, 0);
 		GL11.glScalef(0.75F, 0.75F, 0.75F);
-		fontRenderer.drawSplitString(I18n.format("gui.packmissing.modlink"),0, 0, 320, Color.BLACK.getRGB());
+		MTSFontRenderer.drawEssay(I18n.format("gui.packmissing.modlink"), 0, 0, Color.BLACK, 320);
 		GL11.glPopMatrix();
 		
 		GL11.glPushMatrix();
-		GL11.glTranslatef(guiLeft + 10, guiTop + 115, 0);
+		GL11.glTranslatef(guiTextureLeft + 10, guiTextureTop + 115, 0);
 		GL11.glScalef(0.75F, 0.75F, 0.75F);
-		fontRenderer.drawSplitString(I18n.format("gui.packmissing.misplaced"),0, 0, 320, Color.BLACK.getRGB());
+		MTSFontRenderer.drawEssay(I18n.format("gui.packmissing.misplaced"), 0, 0, Color.BLACK, 320);
 		GL11.glPopMatrix();
 		
 		GL11.glPushMatrix();
-		GL11.glTranslatef(guiLeft + 10, guiTop + 150, 0);
+		GL11.glTranslatef(guiTextureLeft + 10, guiTextureTop + 150, 0);
 		GL11.glScalef(0.75F, 0.75F, 0.75F);
-		fontRenderer.drawSplitString(I18n.format("gui.packmissing.versionerror"),0, 0, 320, Color.BLACK.getRGB());
+		MTSFontRenderer.drawEssay(I18n.format("gui.packmissing.versionerror"), 0, 0, Color.BLACK, 320);
 		GL11.glPopMatrix();
-	}
-	
-	@Override
-	public boolean doesGuiPauseGame(){
-		return false;
 	}
 }
