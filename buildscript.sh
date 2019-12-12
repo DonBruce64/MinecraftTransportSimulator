3ed84495e319b185d77a7ed8103054ff8821018d
DonBruce64 <rapscallion827@gmail.com>
2019-12-11 22:04:36 -0600

    Updated to V15.7.0.

--- buildscript.sh
+++ buildscript.sh
@@ -2,7 +2,7 @@
 VERPREFIX="1.12.2-"
 
 #Get the string of line with the mod version number.
-VERSTRING=$(cat main/java/minecrafttransportsimulator/MTS.java | grep "MODVER")
+VERSTRING=$(cat src/main/java/minecrafttransportsimulator/MTS.java | grep "MODVER")
 #Now parse out the part right after MODVER.
 VERSTRING=${VERSTRING##*MODVER}
 #Now isolate the version.
@@ -10,9 +10,7 @@ VERSTRING=${VERSTRING:2:${#VERSTRING}-4}
 
 #Now that we have the version we need to inject it into the build.gradle file.
 echo "Configuring build.gradle for $VERPREFIX$VERSTRING"
-sed -i '13s/.*version.*/version = "'$VERPREFIX$VERSTRING'"/' ../build.gradle
+sed -i '14s/.*version.*/version = "'$VERPREFIX$VERSTRING'"/' build.gradle
 
 #Finally, build the mod.
-cd ../
 ./gradlew build --offline
-cd src
