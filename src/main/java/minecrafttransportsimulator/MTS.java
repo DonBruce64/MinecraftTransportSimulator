15b1608eec9170292faea9e597682094fa85ebb5
DonBruce64 <rapscallion827@gmail.com>
2020-01-25 15:12:20 -0600

    Updated to 15.10.0.

--- src/main/java/minecrafttransportsimulator/MTS.java
+++ src/main/java/minecrafttransportsimulator/MTS.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class MTS {
 	public static final String MODID="mts";
 	public static final String MODNAME="Minecraft Transport Simulator";
-	public static final String MODVER="15.9.2";
+	public static final String MODVER="15.10.0";
 	
 	@Instance(value = MTS.MODID)
 	public static MTS instance;
