829c3a18530f597d094ea942fc224ccbd693c2f0
DonBruce64 <rapscallion827@gmail.com>
2020-02-07 18:03:33 -0600

    More code for item wrapper.  Still can't use it till pack items are registered on the core mod side.

--- src/main/java/minecrafttransportsimulator/items/core/AItemBase.java
+++ src/main/java/minecrafttransportsimulator/items/core/AItemBase.java
@@ -2,6 +2,7 @@ package minecrafttransportsimulator.items.core;
 
 import java.util.List;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 
 /**Base item class for all MTS items.  Contains multiple methods to define the item's behavior,
@@ -27,10 +28,39 @@ public abstract class AItemBase{
 	public abstract void addTooltipLines(List<String> tooltipLines, NBTTagCompound tag);
 	
 	/**
-	 *  Called when the item is used.  This is either a left or right-click, which is defined
-	 *  by the state of the rightClicked flag.  If this item did something, and you don't want
-	 *  the player to attempt to interact with any blocks they are clicking (or their offhand item)
-	 *  return true.  Otherwise, return false and MC will act like this item doesn't do anything.
+	 *  The return value of this method determines if  {@link #onUsed(double, double, double, boolean)}
+	 *  will be called.  Used to prevent the copious amount of checks for server/client sided
+	 *  interaction and removes the need for proxies as even if there's client-side code in onUsed
+	 *  method the method itself won't be called on the server so we don't need to worry.
 	 */
-	public abstract boolean onUsed(double clickedX, double clickedY, double clickedZ, boolean rightClicked);
+	public abstract InteractionSide getInteractionSide();
+	
+	/**
+	 *  Called when the item is used.  This is either a left or right-click, which is defined
+	 *  by the state of the rightClicked flag.  This method is only called if {@link #getInteractionSide()}
+	 *  has a value not equal to {@link InteractionSide#NONE}.  In this case, this method is called on the
+	 *  appropriate side.  Unlike the regular MC code, this method is called for both clicking of
+	 *  blocks and clicking in the air.  In the case of blocks, the clicked values will be the point
+	 *  on the block clicked.  In the case of the air, it's the player's current position.
+	 */
+	public void onUsed(double clickedX, double clickedY, double clickedZ, EntityPlayer player){}
+	
+	public enum InteractionSide{
+		CLIENT(true, false),
+		SERVER(false, true),
+		BOTH(true, true),
+		NONE(false, false);
+		
+		private final boolean interactClient;
+		private final boolean interactServer;
+		
+		private InteractionSide(boolean interactClient, boolean interactServer){
+			this.interactClient = interactClient;
+			this.interactServer = interactServer;
+		}
+		
+		public boolean interactOn(boolean clientSide){
+			return clientSide ? interactClient : interactServer;
+		}
+	}
 }
--- src/main/java/minecrafttransportsimulator/wrappers/WrapperItem.java
+++ src/main/java/minecrafttransportsimulator/wrappers/WrapperItem.java
@@ -6,15 +6,22 @@ import javax.annotation.Nullable;
 
 import minecrafttransportsimulator.items.core.AItemBase;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 /**Wrapper for MC GUI classes.  Constructor takes a type of {@link AItemBase}, but
  * is only visible when calling {@link #createItem(AItemBase)}.  This will automatically
  * construct the wrapper and will return the created instance of the item (not wrapper)
  * for use in the code.  The wrapper instance is cached and saved to be registered
- * in the MC systems.  When interfacing with MC items, ALWAYS use this class!
+ * in the MC systems.  When interfacing with MC systems use this class, but when
+ * doing code in MTS use the item, NOT the wrappoer!
  *
  * @author don_bruce
  */
@@ -48,6 +55,46 @@ public class WrapperItem extends Item{
 	@Override
 	public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltipLines, ITooltipFlag flagIn){
 		item.addTooltipLines(tooltipLines, stack.getTagCompound());
+	}
+	
+	/**
+	 *  This is called by the main MC system when this item is used.  Can either be main-hand or offhand.
+	 *  Whether or not we forward the call to the item depends on the value of {@link AItemBase#getInteractionSide()}.
+	 *  Note that this won't get called if the player has clicked a block or entity.  In the case of the entity, it gets
+	 *  the call itself.  In the case of the block, we get the call on the method after this one.
+	 */
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand){
+		if(item.getInteractionSide().equals(AItemBase.InteractionSide.NONE)){
+			//Treat the item as a regular nothing item.  Return pass to let MC do what it does.
+			return new ActionResult<ItemStack>(EnumActionResult.PASS, player.getHeldItem(hand));
+		}else{
+			if(item.getInteractionSide().interactOn(world.isRemote)){
+				item.onUsed(player.posX, player.posY, player.posZ, player);
+			}
+			//Return FAIL to prevent MC from doing other interaction bits as we should be doing interaction.
+			return new ActionResult<ItemStack>(EnumActionResult.FAIL, player.getHeldItem(hand));
+		}
+	}
+	
+	/**
+	 *  This is called by the main MC system when this item is used on a block.  Can either be main-hand or offhand.
+	 *  Whether or not we forward the call to the item depends on the value of {@link AItemBase#getInteractionSide()}.
+	 *  Note that if this or the block returns success, processing will stop.
+	 */
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
+		//TODO we may not need this method if we don't care about block-specific item interaction.
+		if(item.getInteractionSide().equals(AItemBase.InteractionSide.NONE)){
+			//Treat the item as a regular nothing item.  Return pass to let MC do what it does.
+			return EnumActionResult.PASS;
+		}else{
+			if(item.getInteractionSide().interactOn(world.isRemote)){
+				item.onUsed(hitX, hitY, hitZ, player);
+			}
+			//Return FAIL to prevent MC from doing other interaction bits as we should be doing interaction.
+			return EnumActionResult.FAIL;
+		}
 	}
 	
 	
