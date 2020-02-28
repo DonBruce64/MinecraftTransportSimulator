2445db719ece7df2861aa673245df5b03185abd3
DonBruce64 <rapscallion827@gmail.com>
2020-02-27 22:13:23 -0600

    Removed un-used imports.

--- src/main/java/minecrafttransportsimulator/guis/GUIInstruments.java
+++ src/main/java/minecrafttransportsimulator/guis/GUIInstruments.java
@@ -2,11 +2,7 @@ package minecrafttransportsimulator.guis;
 
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import org.lwjgl.opengl.GL11;
