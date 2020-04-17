f16f13ed80d32e0393cdec44801e2ec33e3c6dd4
DonBruce64 <rapscallion827@gmail.com>
2020-04-16 19:57:09 -0500

    Fixed wrong comment.

--- src/main/java/minecrafttransportsimulator/blocks/tileentities/instances/TileEntityPole_TrafficSignal.java
+++ src/main/java/minecrafttransportsimulator/blocks/tileentities/instances/TileEntityPole_TrafficSignal.java
@@ -3,7 +3,7 @@ package minecrafttransportsimulator.blocks.tileentities.instances;
 import minecrafttransportsimulator.blocks.tileentities.components.ATileEntityPole_Component;
 import minecrafttransportsimulator.jsondefs.JSONPoleComponent;
 
-/**Crossing signal component for poles.  This doesn't tick, as the state of the light
+/**Traffic signal component for poles.  This doesn't tick, as the state of the light
  * is by default a flashing red until changed by a {@link TileEntitySignalController}.
  * 
  * @author don_bruce
