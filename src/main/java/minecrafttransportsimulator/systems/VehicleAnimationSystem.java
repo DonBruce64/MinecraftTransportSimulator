65dc416e4cf8f50a5ce6bee66c1709f2b3f62878
DonBruce64 <rapscallion827@gmail.com>
2020-09-21 22:07:52 -0500

    Added engine hours variable.  Addresses #492.

--- src/main/java/minecrafttransportsimulator/systems/VehicleAnimationSystem.java
+++ src/main/java/minecrafttransportsimulator/systems/VehicleAnimationSystem.java
@@ -109,6 +109,7 @@ public final class VehicleAnimationSystem{
 					case("engine_magneto"): return engine.state.magnetoOn ? 1 : 0;
 					case("engine_starter"): return engine.state.esOn ? 1 : 0;
 					case("engine_jumper_cable"): return engine.linkedEngine != null ? 1 : 0;
+					case("engine_hours"): return engine.hours;
 				}
 			}else if(optionalPart instanceof PartGun){
 				PartGun gun = (PartGun) optionalPart;
