1e6fbb9d6c522fa3af8fe72ae8a1fb3fa557606d
DonBruce64 <rapscallion827@gmail.com>
2020-10-30 23:40:02 -0500

    Updated to V19.8.0.

--- src/main/java/minecrafttransportsimulator/mcinterface/MasterLoader.java
+++ src/main/java/minecrafttransportsimulator/mcinterface/MasterLoader.java
@@ -53,7 +53,7 @@ import minecrafttransportsimulator.systems.PackParserSystem;
 public class MasterLoader{
 	public static final String MODID = "mts";
 	public static final String MODNAME = "Minecraft Transport Simulator";
-	public static final String MODVER = "19.8.0-BETA6";
+	public static final String MODVER = "19.8.0";
 	
 	public static String resourceDomain;
 	public static IInterfaceAudio audioInterface;
