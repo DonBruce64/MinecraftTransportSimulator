5a95acc3ccc70f8c66d3995932296f419933dce1
DonBruce64 <rapscallion827@gmail.com>
2020-12-27 22:27:09 -0600

    Added tick sin and cos variables.

--- src/main/java/minecrafttransportsimulator/rendering/components/VehicleAnimations.java
+++ src/main/java/minecrafttransportsimulator/rendering/components/VehicleAnimations.java
@@ -241,6 +241,8 @@ public final class VehicleAnimations{
 		switch(variable){
 			//Vehicle world state cases.
 			case("tick"): return vehicle.world.getTick();
+			case("tick_sin"): return Math.sin(Math.toRadians(vehicle.world.getTick()));
+			case("tick_cos"): return Math.cos(Math.toRadians(vehicle.world.getTick()));
 			case("time"): return vehicle.world.getTime();
 			case("yaw"): return vehicle.angles.y;
 			case("heading"): int heading = (int)-vehicle.angles.y; if(ConfigSystem.configObject.clientControls.north360.value) heading += 180; while (heading < 1) heading += 360; while (heading > 360) heading -= 360; return heading;
