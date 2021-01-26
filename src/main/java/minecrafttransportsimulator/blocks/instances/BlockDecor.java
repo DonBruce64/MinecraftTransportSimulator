c6904a8b57c94b501c479f349027a41829d418f5
DonBruce64 <rapscallion827@gmail.com>
2021-01-25 19:37:32 -0600

    Made decors not open their GUI when holding a paint gun.  Addresses #693.

--- src/main/java/minecrafttransportsimulator/blocks/instances/BlockDecor.java
+++ src/main/java/minecrafttransportsimulator/blocks/instances/BlockDecor.java
@@ -6,6 +6,7 @@ import minecrafttransportsimulator.blocks.tileentities.instances.TileEntityDecor
 import minecrafttransportsimulator.guis.components.InterfaceGUI;
 import minecrafttransportsimulator.guis.instances.GUIPartBench;
 import minecrafttransportsimulator.guis.instances.GUITextEditor;
+import minecrafttransportsimulator.items.instances.ItemPaintGun;
 import minecrafttransportsimulator.mcinterface.WrapperNBT;
 import minecrafttransportsimulator.mcinterface.WrapperPlayer;
 import minecrafttransportsimulator.mcinterface.WrapperWorld;
@@ -19,7 +20,10 @@ public class BlockDecor extends ABlockBaseDecor<TileEntityDecor>{
     @Override
 	public boolean onClicked(WrapperWorld world, Point3i point, Axis axis, WrapperPlayer player){
     	TileEntityDecor decor = (TileEntityDecor) world.getTileEntity(point);
-		if(decor.definition.general.itemTypes != null || decor.definition.general.items != null){
+		if(player.getHeldItem() instanceof ItemPaintGun){
+			//Don't do decor actions if we are holding a paint gun.
+			return false;
+		}else if(decor.definition.general.itemTypes != null || decor.definition.general.items != null){
 			if(world.isClient()){
 				InterfaceGUI.openGUI(new GUIPartBench(decor, player));
 			}
