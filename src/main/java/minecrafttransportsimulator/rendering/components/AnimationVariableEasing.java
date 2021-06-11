c93e2d58c7e8472ef5d092e5fa8b5813ff107c82
Turbo Defender <rishimenon82004@gmail.com>
2021-06-11 18:17:35 +0530

    added quadratic easing formulas

---
+++ src/main/java/minecrafttransportsimulator/rendering/components/AnimationVariableEasing.java
@@ -0,0 +1,71 @@
+package minecrafttransportsimulator.rendering.components;
+
+import minecrafttransportsimulator.jsondefs.JSONAnimationDefinition;
+import minecrafttransportsimulator.rendering.components.DurationDelayClock;
+
+public class AnimationVariableEasing {
+
+	public double getEasingValue(JSONAnimationDefinition animation, long timeMoved, double value) {
+		if (animation.easing != null) {
+			switch(animation.easing) {
+				case EASEINQUAD: {
+					return easeInQuad(animation, timeMoved, value);
+				}
+				
+				case EASEOUTQUAD: {
+					return easeOutQuad(animation, timeMoved, value);
+				}
+				
+				case EASEINOUTQUAD: {
+					return easeInOutQuad(animation, timeMoved, value);
+				}
+				
+				default: {
+					return timeMoved/(double)(animation.duration*50);
+				}
+			}
+			
+		} else {
+			
+			return timeMoved/(double)(animation.duration*50);
+			
+		}
+	}
+	
+	public double easeInQuad(JSONAnimationDefinition animation, long timeElapsed, double endValue) {
+		long time = timeElapsed;
+		long duration = animation.duration*50;
+		time = time/duration;
+		double value = endValue * time * time + 0;
+		System.out.format("Variable: %s | Value: %f | EndValue: %f | ", animation.variable, value, endValue);
+		System.out.format("Duration: " + duration + " | " + "Time Elapsed: " + time + "\n");
+		
+		return value;
+	}
+	
+	public double easeOutQuad(JSONAnimationDefinition animation, long timeElapsed, double endValue) {
+		long time = timeElapsed;
+		long duration = animation.duration*50;
+		time = time/duration;
+		double value = -endValue * time * (time-2) + 0;
+		
+		return value;
+	}
+	
+	public double easeInOutQuad(JSONAnimationDefinition animation, long timeElapsed, double endValue) {
+		long time = timeElapsed;
+		long duration = animation.duration*50;
+		time = time/(duration/2);
+		
+		if (time < 1) {
+			double value = endValue/2 * time * time + 0;
+			return value;
+		}
+		
+		time--;
+		
+		double value = -endValue/2 * (time * (time - 2) - 1) + 0;
+		return value;
+	}
+
+}
