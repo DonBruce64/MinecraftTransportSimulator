f0e15054b4e1d6ede2543c53d5f26e293fda4420
DonBruce64 <rapscallion827@gmail.com>
2021-09-11 23:59:16 -0500

    Blocked export of internal JSON fields.

--- src/main/java/minecrafttransportsimulator/jsondefs/AJSONItem.java
+++ src/main/java/minecrafttransportsimulator/jsondefs/AJSONItem.java
@@ -18,17 +18,17 @@ public abstract class AJSONItem{
 	/**A String containing any prefix folders for this item.  Used by the packloading system to tell
 	 * where resources are relative to the item's JSON definition file path.  This contains all folders
 	 * "between" the item's classification directory and the item's actual file.*/
-	public String prefixFolders;
+	public transient String prefixFolders;
 	/**ID of the pack that this JSON was pulled from.  Set after JSON is parsed into an object and used
 	 * to tell MTS what jar to load assets from.*/
-	public String packID;
+	public transient String packID;
 	/**Unique name (per-pack) that is used to identify this item in the MTS system.  Set after JSON is 
 	 * parsed into an object and used for lookup operations or for maps when we need to keep track of
 	 * a pack component by name rather than an object instance.  Used heavily in save/load operations.*/
-	public String systemName;
+	public transient String systemName;
 	/**Classification for this item.  This is used to determine what folders this JSON definition,
 	 * and it's associated resources, are found in.*/
-	public ItemClassification classification;
+	public transient ItemClassification classification;
 	/**Generic variable used to represent the general properties of this pack item.  Extend this and
 	 * reference it in the generic for all sub-classes.*/
 	@JSONRequired
