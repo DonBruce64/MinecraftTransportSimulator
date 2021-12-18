7523a5d312034d7afc250c9e06a7370817f13361
DonBruce64 <rapscallion827@gmail.com>
2021-12-17 21:15:46 -0600

    Updated to 20.4.0.

--- src/main/java/minecrafttransportsimulator/MasterLoader.java
+++ src/main/java/minecrafttransportsimulator/MasterLoader.java
@@ -39,7 +39,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class MasterLoader{
 	public static final String MODID = "mts";
 	public static final String MODNAME = "Minecraft Transport Simulator";
-	public static final String MODVER = "20.4.0-BETA16";
+	public static final String MODVER = "20.4.0";
 	
 	public static Logger logger;
 	public static String resourceDomain;
