a70d61ee5a28d4f8e44a728eaa7cbf7b3d9cb642
DonBruce64 <rapscallion827@gmail.com>
2022-01-16 12:44:29 -0600

    Updated beta number.

--- src/main/java/minecrafttransportsimulator/MasterLoader.java
+++ src/main/java/minecrafttransportsimulator/MasterLoader.java
@@ -39,7 +39,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class MasterLoader{
 	public static final String MODID = "mts";
 	public static final String MODNAME = "Minecraft Transport Simulator";
-	public static final String MODVER = "20.5.0-BETA22";
+	public static final String MODVER = "20.5.0-BETA23";
 	
 	public static Logger logger;
 	public static String resourceDomain;
