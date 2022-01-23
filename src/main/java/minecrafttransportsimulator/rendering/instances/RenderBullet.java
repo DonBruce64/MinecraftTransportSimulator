fb8176bee0dcf950de3a4e6d65b4acdba123192a
DonBruce64 <rapscallion827@gmail.com>
2022-01-23 01:55:30 -0600

    Made slight math improvement to box math.

--- src/main/java/minecrafttransportsimulator/rendering/instances/RenderBullet.java
+++ src/main/java/minecrafttransportsimulator/rendering/instances/RenderBullet.java
@@ -1,13 +1,9 @@
 package minecrafttransportsimulator.rendering.instances;
 
-import minecrafttransportsimulator.baseclasses.Matrix4dPlus;
 import minecrafttransportsimulator.entities.instances.EntityBullet;
 import minecrafttransportsimulator.rendering.components.ARenderEntityDefinable;
 
 public class RenderBullet extends ARenderEntityDefinable<EntityBullet>{
 
-	@Override
-	protected void renderBoundingBoxes(EntityBullet entity, Matrix4dPlus transform){
-		entity.boundingBox.renderWireframe(entity, transform, null);
-	}
+
 }
