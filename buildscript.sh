9591dd74b1b4b6dff7cd68a39ef124a28a438301
Recolic Keghart <root@recolic.net>
2022-04-11 23:57:24 +0800

    merge

--- buildscript.sh
+++ buildscript.sh
@@ -15,4 +15,12 @@ echo "Configuring build.gradle for $VERPREFIX$VERSTRING"
 sed -i '19s/.*version.*/version = "'$VERPREFIX$VERSTRING'"/' build.gradle
 
 #Finally, build the mod.
-./gradlew build --offline
+if [[ "$PULL_DEPS" != 1 ]]; then
+    # Default behavior: build it with `--offline`
+    ./gradlew build --offline
+else
+    # Set PULL_DEPS=1 to build it without `--offline`
+    ./gradlew build
+fi
+
+exit $?
