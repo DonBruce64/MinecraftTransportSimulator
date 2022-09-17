36b9daee52a284a69602d5cdd56f690ed25bf23c
Elephant1214 <nathangetsmail@gmail.com>
2022-09-17 15:07:42 -0600

    modifications

--- src/main/java/mcinterface1122/MtsLoadingPlugin.java
+++ src/main/java/mcinterface1122/MtsLoadingPlugin.java
@@ -1,6 +1,5 @@
 package mcinterface1122;
 
-import mcinterface1122.InterfaceLoader;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.MixinEnvironment;
@@ -11,7 +10,7 @@ import java.util.Map;
 
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 @IFMLLoadingPlugin.Name("Immersive Vehicles")
-public class MtsCoreMod implements IFMLLoadingPlugin {
+public class MtsLoadingPlugin implements IFMLLoadingPlugin {
     @Override
     public String[] getASMTransformerClass() {
         return new String[0];
@@ -32,7 +31,6 @@ public class MtsCoreMod implements IFMLLoadingPlugin {
     public void injectData(Map<String, Object> data) {
         MixinBootstrap.init();
         Mixins.addConfiguration(InterfaceLoader.MODID + ".mixins.json");
-        MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
     }
 
     @Override
