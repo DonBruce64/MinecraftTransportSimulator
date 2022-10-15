10473363a0c8e9537e76748a5c3ff1014fe3ad1d
DonBruce64 <rapscallion827@gmail.com>
2022-10-15 00:20:28 -0500

    Made trailers respect scaling for initial hitch connections.

--- mccore/src/main/java/minecrafttransportsimulator/entities/components/AEntityG_Towable.java
+++ mccore/src/main/java/minecrafttransportsimulator/entities/components/AEntityG_Towable.java
@@ -353,10 +353,10 @@ public abstract class AEntityG_Towable<JSONDefinition extends AJSONPartProvider>
                                 //Iterate though connections on the groups to find two that match types and are close enough.
                                 for (int hitchConnectionIndex = 0; hitchConnectionIndex < hitchConnectionGroup.connections.size(); ++hitchConnectionIndex) {
                                     JSONConnection hitchConnection = hitchConnectionGroup.connections.get(hitchConnectionIndex);
-                                    Point3D hitchPos = hitchConnection.pos.copy().rotate(hitchConnectionDefiner.orientation).add(hitchConnectionDefiner.position);
+                                    Point3D hitchPos = hitchConnection.pos.copy().multiply(hitchConnectionDefiner.scale).rotate(hitchConnectionDefiner.orientation).add(hitchConnectionDefiner.position);
                                     for (int hookupConnectionIndex = 0; hookupConnectionIndex < hookupConnectionGroup.connections.size(); ++hookupConnectionIndex) {
                                         JSONConnection hookupConnection = hookupConnectionGroup.connections.get(hookupConnectionIndex);
-                                        Point3D hookupPos = hookupConnection.pos.copy().rotate(hookupConnectionDefiner.orientation).add(hookupConnectionDefiner.position);
+                                        Point3D hookupPos = hookupConnection.pos.copy().multiply(hookupConnectionDefiner.scale).rotate(hookupConnectionDefiner.orientation).add(hookupConnectionDefiner.position);
 
                                         //Check if we are somewhat close before we check matching type.
                                         //This allows us to warn players that that hitch they are trying to connect to (close to)
