92e809fd21fa589f1f8fe5e917c8aaad58435ab0
DrprofLuigi <brainstain.cool@gmail.com>
2022-12-26 19:48:03 -0800

    Modified canBeClicked() to account for parent parts that are not interactable.

--- mccore/src/main/java/minecrafttransportsimulator/entities/instances/APart.java
+++ mccore/src/main/java/minecrafttransportsimulator/entities/instances/APart.java
@@ -339,7 +339,7 @@ public abstract class APart extends AEntityF_Multipart<JSONPart> {
 
     @Override
     public boolean canBeClicked() {
-        return entityOn.isVariableListTrue(placementDefinition.interactableVariables);
+        return entityOn.isVariableListTrue(placementDefinition.interactableVariables) ? entityOn.canBeClicked() : false;
     }
 
     /**
