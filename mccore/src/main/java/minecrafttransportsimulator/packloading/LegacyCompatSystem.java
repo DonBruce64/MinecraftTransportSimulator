9c695a1e7da836502f0f593bf187a9e362fda193
conman180 <connor.thomas358@gmail.com>
2023-07-05 12:13:35 -0400

    LegacyCompat for seeker heads

--- mccore/src/main/java/minecrafttransportsimulator/packloading/LegacyCompatSystem.java
+++ mccore/src/main/java/minecrafttransportsimulator/packloading/LegacyCompatSystem.java
@@ -1755,11 +1755,14 @@ public final class LegacyCompatSystem {
             definition.bullet.damage = definition.bullet.diameter / 5F;
         }
         //Make guided bullets default to active guidance
-        //Also gives default seeker cone of 45 degrees
         if (definition.bullet.guidanceType == null && definition.bullet.turnRate > 0) {
             definition.bullet.guidanceType = JSONBullet.GuidanceType.ACTIVE;
-            definition.bullet.seekerRange = 1024;
+        }
+        if (definition.bullet.seekerMaxAngle == 0) {
             definition.bullet.seekerMaxAngle = 45;
+        }
+        if (definition.bullet.seekerRange == 0) {
+            definition.bullet.seekerRange = 1024;
         }
 
         //Make rendering particle section for bullets for block hitting if it doesn't exist.
