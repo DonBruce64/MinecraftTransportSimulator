9a4a4865e74f03604e0e74cb82cd3b4e14eb662b
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2023-07-10 19:44:58 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "Immersive Vehicles"
-include("mccore", "mcinterfaceforge1122", "mcinterfaceforge1165")
+include("mccore", "mcinterfaceforge1122", "mcinterfaceforge1165", "mcinterfaceforge1182")
