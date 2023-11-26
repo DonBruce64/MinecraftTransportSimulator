382d3637559f227ec5d8b513f0017d014f2962c8
CC <53581325+boot2big@users.noreply.github.com>
2023-11-26 10:47:59 -0500

    Fixed it the proper way, with advice from Don
    Java makes me feel stupid...

--- mccore/src/main/java/minecrafttransportsimulator/entities/instances/PartGun.java
+++ mccore/src/main/java/minecrafttransportsimulator/entities/instances/PartGun.java
@@ -1177,9 +1177,7 @@ public class PartGun extends APart {
             case ("gun_ammo_count"):
                 return bulletsLeft;
             case ("gun_ammo_count_reloading"):
-                return reloadTimeRemaining > 0 ? reloadingBullet.definition.bullet.quantity : 0; //EEEEVERYTHING YOU KNOW IS WRONG, BLACK IS WHITE UP IS DOWN AND INTS CAN'T 'null'!!! -cbc
-                //I would think that ideally this should've looked like the following, but apparently that's not actually possible, nor was it the solution anyways.
-                //return reloadingBullet.definition.bullet.quantity != null ? 1 : 0;
+                return reloadingBullet != null ? reloadingBullet.definition.bullet.quantity : 0;
             case ("gun_ammo_percent"):
                 return bulletsLeft / definition.gun.capacity;
             case ("gun_active_muzzlegroup"):
