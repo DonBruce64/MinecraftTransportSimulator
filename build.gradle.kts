ac8920af3a67b87378adb77146d4fa0f601642e7
Elephant1214 <nathangetsmail@gmail.com>
2023-12-16 15:37:46 -0700

    Finally fixed the build script and updated Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,15 @@
 import java.nio.file.Paths
-import kotlin.io.path.moveTo
 import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.createDirectories
+import kotlin.io.path.exists
+import kotlin.io.path.moveTo
 
 
-plugins {
-    java
-    kotlin("jvm") version "1.7.20"
-}
+plugins {}
 
-repositories {
-    mavenCentral()
-}
+repositories {}
 
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-}
+dependencies {}
 
 subprojects {
     apply(plugin = "java")
@@ -22,53 +17,37 @@ subprojects {
 
 var modVersion: String = project.property("global_version").toString()
 
-//var modVersion: String = providers.gradleProperty("global_version")
-
 var mcCore = project(":mccore")
 var mcInterfaceForge1122 = project(":mcinterfaceforge1122")
 var mcInterfaceForge1165 = project(":mcinterfaceforge1165")
 
 tasks.register("buildCore") {
-    dependsOn(mcCore.tasks.build)
-    doLast {
-        moveToOut(mcCore, "core")
-    }
+    group = "mts"
+    dependsOn(mcCore.tasks.getByName("build"))
 }
 
-tasks.register("buildForge1122") {
-    doFirst { preBuild() }
-    doLast {
-        moveToOut(mcInterfaceForge1122, "1.12.2")
-    }
-    dependsOn(mcInterfaceForge1122.tasks.build)
+tasks.register<JavaCompile>("buildForge1122") {
+    group = "mts"
+    doLast { moveToOut(mcInterfaceForge1122) }
+    dependsOn(mcInterfaceForge1122.tasks.getByName("build"))
 }
 
-tasks.register("buildForge1165") {
-    doFirst { preBuild() }
-    doLast {
-        moveToOut(mcInterfaceForge1165, "1.16.5")
-    }
-    dependsOn(mcInterfaceForge1165.tasks.build)
+tasks.register<JavaCompile>("buildForge1165") {
+    group = "mts"
+    doLast { moveToOut(mcInterfaceForge1165) }
+    dependsOn(mcInterfaceForge1165.tasks.getByName("build"))
 }
 
-tasks.register("buildForgeAll") {
+tasks.register<JavaCompile>("buildForgeAll") {
+    group = "mts"
     dependsOn(tasks.getByName("buildForge1122"))
     dependsOn(tasks.getByName("buildForge1165"))
 }
 
 @OptIn(ExperimentalPathApi::class)
-fun moveToOut(subProject: Project, versionStr: String) {
+fun moveToOut(subProject: Project) {
     val jarName = "Immersive Vehicles-${subProject.version}.jar"
-    Paths.get("${subProject.projectDir.canonicalPath}/build/libs/$jarName")
-        .moveTo(Paths.get("${project.projectDir.canonicalPath}/out/$jarName"), true)
-}
-
-fun preBuild() {
-    // Could probably be better somehow, but I'm not sure how
-    project.projectDir.canonicalFile.walk()
-        .filter { it.name == "gradle.properties" || it.name == "mcmod.info" || it.name == "InterfaceLoader.java" }
-        .forEach { it.writeText(it.readText()
-            .replace(Regex("mod_version=(.+)"), "mod_version=$modVersion")
-            .replace(Regex("\"version\": \"[^\"]*\""), "\"version\": \"$modVersion\"")
-            .replace(Regex("MODVER = \"[^\"]*\";"), "MODVER = \"$modVersion\";")) }
+    val outPath = Paths.get("${project.projectDir.canonicalPath}/out/")
+    if (!outPath.exists()) outPath.createDirectories()
+    Paths.get("${subProject.projectDir.canonicalPath}/build/libs/$jarName").moveTo(Paths.get(outPath.toString(), jarName), true)
 }
