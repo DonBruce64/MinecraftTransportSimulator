dbbc33807d45693d135f64b44d64f5c2fb684a71
DonBruce64 <rapscallion827@gmail.com>
2023-12-27 17:40:48 -0600

    Modified container code to not use Vanilla interface that can confuse some mods.

--- mcinterfaceforge1122/src/main/java/mcinterface1122/BuilderTileEntityInventoryContainer.java
+++ mcinterfaceforge1122/src/main/java/mcinterface1122/BuilderTileEntityInventoryContainer.java
@@ -4,8 +4,6 @@ import javax.annotation.Nullable;
 
 import minecrafttransportsimulator.blocks.tileentities.components.ATileEntityBase;
 import minecrafttransportsimulator.blocks.tileentities.components.ITileEntityInventoryProvider;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
@@ -18,35 +16,15 @@ import net.minecraftforge.items.ItemHandlerHelper;
  *
  * @author don_bruce
  */
-public class BuilderTileEntityInventoryContainer<InventoryTileEntity extends ATileEntityBase<?> & ITileEntityInventoryProvider> extends BuilderTileEntity<InventoryTileEntity> implements IInventory, IItemHandler {
+public class BuilderTileEntityInventoryContainer<InventoryTileEntity extends ATileEntityBase<?> & ITileEntityInventoryProvider> extends BuilderTileEntity<InventoryTileEntity> implements IItemHandler {
 
     public BuilderTileEntityInventoryContainer() {
         super();
     }
 
     @Override
-    public String getName() {
-        return "item_loader";
-    }
-
-    @Override
-    public boolean hasCustomName() {
-        return false;
-    }
-
-    @Override
-    public int getSizeInventory() {
-        return tileEntity != null ? tileEntity.getInventory().getSize() : 0;
-    }
-
-    @Override
     public int getSlots() {
-        return getSizeInventory();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return tileEntity != null ? tileEntity.getInventory().getCount() == 0 : false;
+        return tileEntity != null ? tileEntity.getInventory().getSize() : 0;
     }
 
     @Override
@@ -54,112 +32,51 @@ public class BuilderTileEntityInventoryContainer<InventoryTileEntity extends ATi
         return tileEntity != null ? ((WrapperItemStack) tileEntity.getInventory().getStack(index)).stack : ItemStack.EMPTY;
     }
 
-
     @Override
-    public ItemStack decrStackSize(int index, int count) {
+    public ItemStack extractItem(int slot, int amount, boolean simulate) {
         if (tileEntity != null) {
-            tileEntity.getInventory().removeFromSlot(index, count);
-            return getStackInSlot(index);
+            ItemStack stack = getStackInSlot(slot);
+            if (stack.getCount() < amount) {
+                amount = stack.getCount();
+            }
+            ItemStack extracted = stack.copy();
+            extracted.setCount(amount);
+            if (!simulate) {
+                stack.setCount(stack.getCount() - amount);
+                tileEntity.getInventory().setStack(new WrapperItemStack(stack), slot);
+            }
+            return extracted;
         } else {
             return ItemStack.EMPTY;
         }
     }
 
     @Override
-    public ItemStack removeStackFromSlot(int index) {
-        if (tileEntity != null) {
-            tileEntity.getInventory().removeFromSlot(index, tileEntity.getInventory().getStack(index).getSize());
-        }
-        return ItemStack.EMPTY;
-    }
-
-    @Override
-    public ItemStack extractItem(int slot, int amount, boolean simulate) {
-        ItemStack stack = getStackInSlot(slot);
-        if (stack.getCount() < amount) {
-            amount = stack.getCount();
-        }
-        ItemStack extracted = stack.copy();
-        extracted.setCount(amount);
-        if (!simulate) {
-            stack.setCount(stack.getCount() - amount);
-            setInventorySlotContents(slot, stack);
-        }
-        return extracted;
-    }
-
-    @Override
-    public void setInventorySlotContents(int index, ItemStack stack) {
-        if (tileEntity != null) {
-            tileEntity.getInventory().setStack(new WrapperItemStack(stack), index);
-        }
-    }
-
-    @Override
     public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
-        ItemStack existingStack = getStackInSlot(slot);
-        if (ItemHandlerHelper.canItemStacksStack(stack, existingStack)) {
-            int amount = existingStack.getMaxStackSize() - existingStack.getCount();
-            if (amount != 0) {
-                if (amount > stack.getCount()) {
-                    amount = stack.getCount();
+        if (tileEntity != null) {
+            ItemStack existingStack = getStackInSlot(slot);
+            if (ItemHandlerHelper.canItemStacksStack(stack, existingStack)) {
+                int amount = existingStack.getMaxStackSize() - existingStack.getCount();
+                if (amount != 0) {
+                    if (amount > stack.getCount()) {
+                        amount = stack.getCount();
+                    }
+                    ItemStack remainderStack = stack.copy();
+                    remainderStack.setCount(remainderStack.getCount() - amount);
+                    if (!simulate) {
+                        existingStack.setCount(existingStack.getCount() + amount);
+                        tileEntity.getInventory().setStack(new WrapperItemStack(existingStack), slot);
+                    }
+                    return remainderStack;
                 }
-                ItemStack remainderStack = stack.copy();
-                remainderStack.setCount(remainderStack.getCount() - amount);
-                if (!simulate) {
-                    existingStack.setCount(existingStack.getCount() + amount);
-                    setInventorySlotContents(slot, existingStack);
-                }
-                return remainderStack;
             }
         }
         return stack;
     }
 
     @Override
-    public int getInventoryStackLimit() {
-        return 64;
-    }
-
-    @Override
     public int getSlotLimit(int slot) {
-        return getInventoryStackLimit();
-    }
-
-    @Override
-    public boolean isUsableByPlayer(EntityPlayer player) {
-        return true;
-    }
-
-    @Override
-    public void openInventory(EntityPlayer player) {
-    }
-
-    @Override
-    public void closeInventory(EntityPlayer player) {
-    }
-
-    @Override
-    public boolean isItemValidForSlot(int index, ItemStack stack) {
-        return true;
-    }
-
-    @Override
-    public int getField(int id) {
-        return 0;
-    }
-
-    @Override
-    public void setField(int id, int value) {
-    }
-
-    @Override
-    public int getFieldCount() {
-        return 0;
-    }
-
-    @Override
-    public void clear() {
+        return 64;
     }
 
     @Override
