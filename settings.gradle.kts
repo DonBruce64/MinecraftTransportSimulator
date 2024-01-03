473a144e10a86596dc90759f44c9fabb8b24d8d6
Elephant1214 <nathangetsmail@gmail.com>
2024-01-02 18:03:25 -0700

    Modernization-ish and fix a lot of issues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,22 @@
+pluginManagement {
+    pluginManagement {
+        repositories {
+            maven("https://maven.minecraftforge.net/")
+            maven("https://maven.gofancy.wtf/releases") // FancyGradle repo
+            maven("https://maven.parchmentmc.org") // Parchment mappings plugin repo
+            maven("https://repo.spongepowered.org/maven") // Mixin repo
+            gradlePluginPortal()
+        }
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "net.minecraftforge.gradle") {
+                useModule("${requested.id}:ForgeGradle:${requested.version}")
+            }
+        }
+    }
+}
+
 rootProject.name = "Immersive Vehicles"
 include("mccore", "mcinterfaceforge1122", "mcinterfaceforge1165")
