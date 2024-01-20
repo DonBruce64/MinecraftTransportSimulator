cd54925e31b0b4f796649cc572df386a130ca145
DonBruce64 <rapscallion827@gmail.com>
2024-01-20 16:11:09 -0600

    Fixed explosion block breaking happening on clients.

--- mcinterfaceforge1122/src/main/java/mcinterface1122/WrapperWorld.java
+++ mcinterfaceforge1122/src/main/java/mcinterface1122/WrapperWorld.java
@@ -966,6 +966,8 @@ public class WrapperWorld extends AWrapperWorld {
             if (event.phase.equals(Phase.START)) {
                 beginProfiling("MTS_ServerVehicleUpdates", true);
                 tickAll();
+                //Also tick active explosions.
+                Explosion.tickActiveExplosions();
 
                 for (EntityPlayer player : event.world.playerEntities) {
                     UUID playerUUID = player.getUniqueID();
