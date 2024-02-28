373aacc1bbe363485aa7bb6bfa63da8695afda49
DonBruce64 <rapscallion827@gmail.com>
2024-02-28 08:21:54 -0600

    Fixed guns rendering when a spectator.

--- mccore/src/main/java/minecrafttransportsimulator/entities/instances/EntityPlayerGun.java
+++ mccore/src/main/java/minecrafttransportsimulator/entities/instances/EntityPlayerGun.java
@@ -110,6 +110,14 @@ public class EntityPlayerGun extends AEntityF_Multipart<JSONDummyPartProvider> {
         super.update();
         //Make sure player is still valid and haven't left the server.
         if (player != null && player.isValid()) {
+        	//Check if the player is specator, if so, we don't do any gun logic.
+        	if(player.isSpectator()) {
+        		if (activeGun != null) {
+                    saveGun(true);
+                }
+        		return;
+        	}
+        	
             //Set our position to the player's position.  We may update this later if we have a gun.
             //We can't update position without the gun as it has an offset defined in it.
             position.set(player.getPosition());
