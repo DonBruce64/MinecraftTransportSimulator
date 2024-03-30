988bad60bfbc35f3d8fa28ab3bb2a4780341e51f
conman180 <connor.thomas358@gmail.com>
2024-03-30 17:39:33 -0400

    Add variables for X and Z coordinates

--- mccore/src/main/java/minecrafttransportsimulator/entities/instances/EntityVehicleF_Physics.java
+++ mccore/src/main/java/minecrafttransportsimulator/entities/instances/EntityVehicleF_Physics.java
@@ -822,6 +822,10 @@ public class EntityVehicleF_Physics extends AEntityVehicleE_Powered {
                 return orientation.angles.z;
             case ("altitude"):
                 return position.y;
+            case ("posX"):
+                return position.x;
+            case ("posZ"):
+                return position.z;
             case ("speed"):
                 return indicatedSpeed;
             case ("speed_scaled"):
