aeae77671a86b5e538cdc1f438223923ddd8aa4e
DonBruce64 <rapscallion827@gmail.com>
2024-07-20 19:23:24 -0500

    Fixed tones not pulling if parts were on subparts.

--- mccore/src/main/java/minecrafttransportsimulator/entities/instances/APart.java
+++ mccore/src/main/java/minecrafttransportsimulator/entities/instances/APart.java
@@ -379,11 +379,20 @@ public abstract class APart extends AEntityF_Multipart<JSONPart> {
     /**
      * Updates the tone of the part to its appropriate type.
      * If the part can't match the tone of this vehicle, then it is not modified.
+     * Code checks first one level up, then the top-level, if the level up doesn't have a tone.
      */
     public void updateTone(boolean recursive) {
         if (placementDefinition.toneIndex != 0) {
+            String partTone = null;
             if (entityOn.subDefinition.partTones != null && entityOn.subDefinition.partTones.size() >= placementDefinition.toneIndex) {
-                String partTone = entityOn.subDefinition.partTones.get(placementDefinition.toneIndex - 1);
+                partTone = entityOn.subDefinition.partTones.get(placementDefinition.toneIndex - 1);
+            }
+            if (partTone == null) {
+                if (masterEntity.subDefinition.partTones != null && masterEntity.subDefinition.partTones.size() >= placementDefinition.toneIndex) {
+                    partTone = masterEntity.subDefinition.partTones.get(placementDefinition.toneIndex - 1);
+                }
+            }
+            if (partTone != null) {
                 for (JSONSubDefinition subDefinition : definition.definitions) {
                     if (subDefinition.subName.equals(partTone)) {
                         updateSubDefinition(partTone);
