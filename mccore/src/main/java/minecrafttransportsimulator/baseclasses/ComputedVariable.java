DonBruce64/MinecraftTransportSimulator

commit f38cb2c96cd4fa22f19e5b2abeddcd8f8d3ce040

Fixed signs with too many text boxes overrunning the text box GUI.  Also fixed sign scaling.

mccore/src/main/java/minecrafttransportsimulator/entities/components/AEntityD_Definable.java
@@ -475,15 +475,13 @@ public abstract class AEntityD_Definable<JSONDefinition extends AJSONMultiModelP
     }
 
     /**
-     * Called to update the text on this entity.  Variable is a map with the key as a field name,
-     * and the value as the value of that field.  Normally just sets the text to the passed-in values,
+     * Called to update the text on this entity.  Normally just sets the text to the passed-in values,
      * but may do supplemental logic if desired.
      */
-    public void updateText(LinkedHashMap<String, String> textLines) {
+    public void updateText(String textKey, String textValue) {
         for (Entry<JSONText, String> textEntry : text.entrySet()) {
-            String newLine = textLines.get(textEntry.getKey().fieldName);
-            if (newLine != null) {
-                textEntry.setValue(newLine);
+            if (textKey.equals(textEntry.getKey().fieldName)) {
+                textEntry.setValue(textValue);
             }
         }
     }

mccore/src/main/java/minecrafttransportsimulator/blocks/tileentities/instances/TileEntityBeacon.java
@@ -1,6 +1,5 @@
 package minecrafttransportsimulator.blocks.tileentities.instances;
 
-import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
 import minecrafttransportsimulator.baseclasses.BoundingBox;
@@ -23,6 +22,9 @@ import minecrafttransportsimulator.mcinterface.IWrapperPlayer;
  */
 public class TileEntityBeacon extends TileEntityDecor {
     public NavBeacon currentBeacon;
+    public static final String BEACON_NAME_TEXT_KEY = "Beacon Name";;
+    public static final String BEACON_GLIDESLOPE_TEXT_KEY = "Glide Slope (Deg)";
+    public static final String BEACON_BEARING_TEXT_KEY = "Bearing (Deg)";
 
     public TileEntityBeacon(AWrapperWorld world, Point3D position, IWrapperPlayer placingPlayer, ItemDecor item, IWrapperNBT data) {
         super(world, position, placingPlayer, item, data);
@@ -41,7 +43,7 @@ public class TileEntityBeacon extends TileEntityDecor {
     }
 
     @Override
-    public void updateText(LinkedHashMap<String, String> textLines) {
+    public void updateText(String textKey, String textValue) {
         if (currentBeacon != null) {
             NavBeacon.removeFromWorld(world, currentBeacon.name);
             currentBeacon = null;
@@ -49,28 +51,44 @@ public class TileEntityBeacon extends TileEntityDecor {
         try {
             //Try to create the beacon before setting text.  If it's invalid text, we don't want to save it.
             //If the object can be created, then we just call super and let it handle this.
-            String name = null;
-            double glideSlope = 0;
-            double bearing = 0;
-            int entry = 0;
-            for (Entry<String, String> textEntry : textLines.entrySet()) {
-                switch (entry++) {
-                    case 0:
-                        name = textEntry.getValue();
-                        break;
-                    case 1:
-                        glideSlope = Double.parseDouble(textEntry.getValue());
-                        break;
-                    case 2:
-                        bearing = Double.parseDouble(textEntry.getValue());
-                        break;
-                    default: //Don't do anything, we don't use this text here.
+            String beaconName = null;
+            String glideslope = null;
+            String bearing = null;
+            for (Entry<JSONText, String> textEntry : text.entrySet()) {
+                String entryKey = textEntry.getKey().fieldName;
+                if (entryKey != null) {
+                    switch (entryKey) {
+                        case BEACON_NAME_TEXT_KEY: {
+                            beaconName = textEntry.getValue();
+                            break;
+                        }
+                        case BEACON_GLIDESLOPE_TEXT_KEY: {
+                            glideslope = textEntry.getValue();
+                            break;
+                        }
+                        case BEACON_BEARING_TEXT_KEY: {
+                            bearing = textEntry.getValue();
+                            break;
+                        }
+                    }
                 }
             }
-
-            NavBeacon newBeacon = new NavBeacon(world, name, glideSlope, bearing, position);
-            super.updateText(textLines);
-            currentBeacon = newBeacon;
+            switch (textKey) {
+                case BEACON_NAME_TEXT_KEY: {
+                    beaconName = textValue;
+                    break;
+                }
+                case BEACON_GLIDESLOPE_TEXT_KEY: {
+                    glideslope = textValue;
+                    break;
+                }
+                case BEACON_BEARING_TEXT_KEY: {
+                    bearing = textValue;
+                    break;
+                }
+            }
+            currentBeacon = new NavBeacon(world, beaconName, Double.parseDouble(glideslope), Double.parseDouble(bearing), position);
+            super.updateText(textKey, textValue);
         } catch (Exception e) {
             //Don't update text.  It's entered invalid.
         }

mccore/src/main/java/minecrafttransportsimulator/entities/components/AEntityF_Multipart.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -411,12 +410,6 @@ public abstract class AEntityF_Multipart<JSONDefinition extends AJSONPartProvide
             stack.setData(stackData);
         }
         return stack;
-    }
-
-    @Override
-    public void updateText(LinkedHashMap<String, String> textLines) {
-        super.updateText(textLines);
-        parts.forEach(part -> part.updateText(textLines));
     }
 
     @Override

mccore/src/main/java/minecrafttransportsimulator/guis/components/GUIComponent3DModel.java
@@ -38,7 +38,7 @@ public class GUIComponent3DModel extends AGUIComponent {
     public final boolean staticScaling;
 
     public boolean spin;
-    public float scale;
+    public float scale = 1.0F;
     public String modelLocation;
     public String textureLocation;
 

mccore/src/main/java/minecrafttransportsimulator/guis/instances/GUITextEditor.java
@@ -2,15 +2,14 @@ package minecrafttransportsimulator.guis.instances;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import minecrafttransportsimulator.baseclasses.ColorRGB;
 import minecrafttransportsimulator.blocks.tileentities.instances.TileEntityPole_Sign;
 import minecrafttransportsimulator.entities.components.AEntityD_Definable;
 import minecrafttransportsimulator.entities.components.AEntityF_Multipart;
-import minecrafttransportsimulator.entities.instances.APart;
 import minecrafttransportsimulator.guis.components.AGUIBase;
 import minecrafttransportsimulator.guis.components.GUIComponent3DModel;
 import minecrafttransportsimulator.guis.components.GUIComponentButton;
@@ -23,107 +22,160 @@ import minecrafttransportsimulator.rendering.RenderText.TextAlignment;
 import minecrafttransportsimulator.systems.LanguageSystem;
 
 public class GUITextEditor extends AGUIBase {
-    //Buttons.
+    private static final int signScale = 32;
+
+    //General components.
+    private final boolean forSigns;
+    private GUIComponent3DModel modelRender;
     private GUIComponentButton confirmButton;
+    private int boxWidth;
+    private int populatingPageHeight;
 
     //Input boxes and their field names.
-    private final Map<String, GUIComponentTextBox> textInputBoxes = new HashMap<>();
+    private final List<Map<String, TextBoxStruct>> textInputBoxes = new ArrayList<>();
+    private GUIComponentButton prevPageButton;
+    private GUIComponentButton nextPageButton;
+    private int currentPage;
 
     //Entity clicked.
     private final AEntityD_Definable<?> entity;
 
     //Labels for sign.  These do fancy rendering.
-    private final Map<GUIComponentLabel, String> signTextLabels = new HashMap<>();
+    private final Map<GUIComponentLabel, GUIComponentTextBox> signTextLabels = new HashMap<>();
 
     public GUITextEditor(AEntityD_Definable<?> entity) {
         super();
         this.entity = entity;
+        this.forSigns = entity instanceof TileEntityPole_Sign;
     }
 
     @Override
     public void setupComponents() {
         super.setupComponents();
-        int boxWidth;
-        List<JSONText> textObjects;
-        List<String> textLines;
-        textInputBoxes.clear();
-        if (entity instanceof TileEntityPole_Sign) {
-            //Add the render to render the sign.
-            GUIComponent3DModel modelRender = new GUIComponent3DModel(guiLeft + 3 * getWidth() / 4, guiTop + 160, 64.0F, false, false, false);
+
+        //First get the model render and box width set, since that's used in text rendering.
+        if (forSigns) {
+            modelRender = new GUIComponent3DModel(guiLeft + 3 * getWidth() / 4, guiTop + 110, signScale, false, false, true);
             addComponent(modelRender);
             modelRender.modelLocation = entity.definition.getModelLocation(entity.subDefinition);
             modelRender.textureLocation = entity.definition.getTextureLocation(entity.subDefinition);
-
-            //Set text and text objects.
             boxWidth = 100;
-            textObjects = new ArrayList<>(entity.text.keySet());
-            textLines = new ArrayList<>(entity.text.values());
-
-            //Add render-able labels for the sign object.
-            signTextLabels.clear();
-            for (byte i = 0; i < textObjects.size(); ++i) {
-                JSONText textDef = textObjects.get(i);
-                GUIComponentLabel label = new GUIComponentLabel(modelRender.constructedX + (int) (textDef.pos.x * 64F), modelRender.constructedY - (int) (textDef.pos.y * 64F), textDef.color, textLines.get(i), TextAlignment.values()[textDef.renderPosition], textDef.scale * 64F / 16F, textDef.wrapWidth * 64 / 16, textDef.fontName, textDef.autoScale);
-                addComponent(label);
-                signTextLabels.put(label, textDef.fieldName);
-            }
         } else {
             boxWidth = 200;
-            textObjects = new ArrayList<>(entity.text.keySet());
-            textLines = new ArrayList<>(entity.text.values());
-
-            //Add part text objects if we are a multipart.
-            if (entity instanceof AEntityF_Multipart) {
-                for (APart part : ((AEntityF_Multipart<?>) entity).allParts) {
-                    textObjects.addAll(part.text.keySet());
-                    textLines.addAll(part.text.values());
-                }
-            }
         }
 
-        //Add text box components for every text.  Paired with labels to render the text name above the boxes.
-        //Don't add multiple boxes per text field, however.  Those use the same box.
-        int currentOffset = 0;
-        for (JSONText textObject : textObjects) {
-            if (textObject.variableName == null && !textInputBoxes.containsKey(textObject.fieldName)) {
-                //No text box present for the field name.  Create a new one.
-                GUIComponentLabel label = new GUIComponentLabel(guiLeft + 20, guiTop + 30 + currentOffset, ColorRGB.BLACK, textObject.fieldName);
-                addComponent(label);
-                int textRowsRequired = 1 + 5 * textObject.maxLength / boxWidth;
-                GUIComponentTextBox box = new GUIComponentTextBox(this, guiLeft + 20, label.constructedY + 10, boxWidth, 12 * textRowsRequired, textLines.get(textObjects.indexOf(textObject)), ColorRGB.WHITE, textObject.maxLength);
-                addComponent(box);
-                textInputBoxes.put(textObject.fieldName, box);
-                currentOffset += box.height + 12;
-            }
+        //Now populate the text boxes.
+        textInputBoxes.clear();
+        signTextLabels.clear();
+
+        //Add text on this entity.
+        addTextFromEntity(entity);
+        if (entity instanceof AEntityF_Multipart) {
+            ((AEntityF_Multipart<?>) entity).allParts.forEach(part -> addTextFromEntity(part));
         }
+
+        //Add navigation buttons.
+        addComponent(prevPageButton = new GUIComponentButton(this, guiLeft + 20, guiTop + 10, 20, 20, "<") {
+            @Override
+            public void onClicked(boolean leftSide) {
+                --currentPage;
+            }
+        });
+        addComponent(nextPageButton = new GUIComponentButton(this, guiLeft + 100, guiTop + 10, 20, 20, ">") {
+            @Override
+            public void onClicked(boolean leftSide) {
+                ++currentPage;
+            }
+        });
 
         //Add the confirm button.
         addComponent(confirmButton = new GUIComponentButton(this, guiLeft + 150, guiTop + 15, 80, 20, LanguageSystem.GUI_CONFIRM.getCurrentValue()) {
             @Override
             public void onClicked(boolean leftSide) {
-                LinkedHashMap<String, String> packetTextLines = new LinkedHashMap<String, String>();
-                for (JSONText textObject : textObjects) {
-                    if (textObject.variableName == null) {
-                        packetTextLines.put(textObject.fieldName, textInputBoxes.get(textObject.fieldName).getText());
-                    }
-                }
-                InterfaceManager.packetInterface.sendToServer(new PacketEntityTextChange(entity, packetTextLines));
+                textInputBoxes.forEach(page -> page.forEach((textKey, textStruct) -> textStruct.entities.forEach(textEntity -> InterfaceManager.packetInterface.sendToServer(new PacketEntityTextChange(textEntity, textKey, textStruct.textBox.text)))));
                 close();
             }
         });
     }
 
+    private void addTextFromEntity(AEntityD_Definable<?> entity) {
+        for (Entry<JSONText, String> textEntry : entity.text.entrySet()) {
+            addSingleTextEntry(entity, textEntry);
+        }
+    }
+
+    private void addSingleTextEntry(AEntityD_Definable<?> entity, Entry<JSONText, String> textEntry) {
+        //First see if we have this text on any page.
+        JSONText textDef = textEntry.getKey();
+        String textKey = textDef.fieldName;
+        if (textKey != null) {
+            for (Map<String, TextBoxStruct> page : textInputBoxes) {
+                if (page.containsKey(textKey)) {
+                    //Just add ourselves to the list.
+                    page.get(textKey).entities.add(entity);
+                    return;
+                }
+            }
+
+            //No entry found, make one at the end of the page.
+            int textRowsRequired = 1 + 5 * textDef.maxLength / boxWidth;
+            int textBoxHeight = 12 * textRowsRequired;
+            int boxSpacing = 12;
+            int boxOffsetFromLabel = 10;
+            int maxLowerLimit = getHeight() - boxSpacing;
+            if (textInputBoxes.isEmpty() || populatingPageHeight + boxSpacing + boxOffsetFromLabel + textBoxHeight > maxLowerLimit) {
+                //Need to add a page.
+                textInputBoxes.add(new HashMap<>());
+                populatingPageHeight = 0;
+            }
+
+            //Create the box and label, then add them to a new struct.
+            GUIComponentLabel textLabel = new GUIComponentLabel(guiLeft + 20, guiTop + 30 + populatingPageHeight, ColorRGB.BLACK, textKey);
+            addComponent(textLabel);
+            GUIComponentTextBox textBox = new GUIComponentTextBox(this, guiLeft + 20, textLabel.constructedY + boxOffsetFromLabel, boxWidth, textBoxHeight, entity.text.get(textDef), ColorRGB.WHITE, textDef.maxLength);
+            addComponent(textBox);
+            textLabel.setComponent(textBox);
+            if (forSigns) {
+                GUIComponentLabel signLabel = new GUIComponentLabel(modelRender.constructedX + (int) (textDef.pos.x * signScale), modelRender.constructedY - (int) (textDef.pos.y * signScale), textDef.color, textBox.text, TextAlignment.values()[textDef.renderPosition], textDef.scale * signScale / 16F, textDef.wrapWidth * signScale / 16, textDef.fontName, textDef.autoScale);
+                addComponent(signLabel);
+                signTextLabels.put(signLabel, textBox);
+            }
+            textInputBoxes.get(textInputBoxes.size() - 1).put(textKey, new TextBoxStruct(textBox, entity));
+            populatingPageHeight += boxOffsetFromLabel + textBox.height + boxSpacing;
+        }
+    }
+
     @Override
     public void setStates() {
         super.setStates();
+        prevPageButton.enabled = currentPage > 0;
+        nextPageButton.enabled = currentPage + 1 < textInputBoxes.size();
         confirmButton.enabled = true;
-        signTextLabels.forEach((label, fieldName) -> {
-            label.text = textInputBoxes.get(fieldName).getText();
-        });
+        for (int i = 0; i < textInputBoxes.size(); ++i) {
+            for (TextBoxStruct textBoxStruct : textInputBoxes.get(i).values()) {
+                textBoxStruct.textBox.visible = i == currentPage;
+            }
+        }
+        signTextLabels.forEach((label, textBox) -> label.text = textBox.getText());
     }
 
     @Override
     protected boolean canStayOpen() {
         return super.canStayOpen() && entity.isValid;
+    }
+
+    @Override
+    public int getWidth() {
+        return super.getWidth();
+    }
+
+    private class TextBoxStruct {
+        private final GUIComponentTextBox textBox;
+        private final List<AEntityD_Definable<?>> entities = new ArrayList<>();
+
+        private TextBoxStruct(GUIComponentTextBox textBox, AEntityD_Definable<?> entity) {
+            this.textBox = textBox;
+            entities.add(entity);
+        }
     }
 }

mccore/src/main/java/minecrafttransportsimulator/packets/instances/PacketEntityTextChange.java
@@ -1,48 +1,42 @@
 package minecrafttransportsimulator.packets.instances;
 
-import java.util.LinkedHashMap;
-
 import io.netty.buffer.ByteBuf;
 import minecrafttransportsimulator.entities.components.AEntityD_Definable;
 import minecrafttransportsimulator.mcinterface.AWrapperWorld;
 import minecrafttransportsimulator.packets.components.APacketEntity;
 
 /**
- * Packet sent to entities to update their text lines.  This is sent from the
+ * Packet sent to entities to update their text.  This is sent from the
  * text GUI to servers to update the text, and then sent back to all clients for syncing.
  *
  * @author don_bruce
  */
 public class PacketEntityTextChange extends APacketEntity<AEntityD_Definable<?>> {
-    private final LinkedHashMap<String, String> textLines;
+    private final String textKey;
+    private final String textValue;
 
-    public PacketEntityTextChange(AEntityD_Definable<?> entity, LinkedHashMap<String, String> textLines) {
+    public PacketEntityTextChange(AEntityD_Definable<?> entity, String textKey, String textValue) {
         super(entity);
-        this.textLines = textLines;
+        this.textKey = textKey;
+        this.textValue = textValue;
     }
 
     public PacketEntityTextChange(ByteBuf buf) {
         super(buf);
-        byte textLineCount = buf.readByte();
-        this.textLines = new LinkedHashMap<>();
-        for (byte i = 0; i < textLineCount; ++i) {
-            textLines.put(readStringFromBuffer(buf), readStringFromBuffer(buf));
-        }
+        this.textKey = readStringFromBuffer(buf);
+        this.textValue = readStringFromBuffer(buf);
     }
 
     @Override
     public void writeToBuffer(ByteBuf buf) {
         super.writeToBuffer(buf);
-        buf.writeByte(textLines.size());
-        textLines.forEach((key, value) -> {
-            writeStringToBuffer(key, buf);
-            writeStringToBuffer(value, buf);
-        });
+        writeStringToBuffer(textKey, buf);
+        writeStringToBuffer(textValue, buf);
     }
 
     @Override
     public boolean handle(AWrapperWorld world, AEntityD_Definable<?> entity) {
-        entity.updateText(textLines);
+        entity.updateText(textKey, textValue);
         return true;
     }
 }

mccore/src/main/java/minecrafttransportsimulator/packloading/LegacyCompatSystem.java
@@ -15,6 +15,7 @@ import minecrafttransportsimulator.baseclasses.Point3D;
 import minecrafttransportsimulator.baseclasses.RotationMatrix;
 import minecrafttransportsimulator.blocks.components.ABlockBase.Axis;
 import minecrafttransportsimulator.blocks.components.ABlockBase.BlockMaterial;
+import minecrafttransportsimulator.blocks.tileentities.instances.TileEntityBeacon;
 import minecrafttransportsimulator.blocks.tileentities.instances.TileEntityRoad.RoadComponent;
 import minecrafttransportsimulator.entities.instances.EntityVehicleF_Physics;
 import minecrafttransportsimulator.entities.instances.PartEngine;
@@ -1786,7 +1787,7 @@ public final class LegacyCompatSystem {
 
                 JSONText nameTextObject = new JSONText();
                 nameTextObject.pos = new Point3D(0, -500, 0);
-                nameTextObject.fieldName = "Beacon Name";
+                nameTextObject.fieldName = TileEntityBeacon.BEACON_NAME_TEXT_KEY;
                 nameTextObject.defaultText = "NONE";
                 nameTextObject.maxLength = 5;
                 nameTextObject.color = ColorRGB.WHITE;
@@ -1794,7 +1795,7 @@ public final class LegacyCompatSystem {
 
                 JSONText glideslopeTextObject = new JSONText();
                 glideslopeTextObject.pos = new Point3D(0, -500, 0);
-                glideslopeTextObject.fieldName = "Glide Slope (Deg)";
+                glideslopeTextObject.fieldName = TileEntityBeacon.BEACON_GLIDESLOPE_TEXT_KEY;
                 glideslopeTextObject.defaultText = "10.0";
                 glideslopeTextObject.maxLength = 5;
                 glideslopeTextObject.color = ColorRGB.WHITE;
@@ -1802,7 +1803,7 @@ public final class LegacyCompatSystem {
 
                 JSONText bearingTextObject = new JSONText();
                 bearingTextObject.pos = new Point3D(0, -500, 0);
-                bearingTextObject.fieldName = "Bearing (Deg)";
+                bearingTextObject.fieldName = TileEntityBeacon.BEACON_BEARING_TEXT_KEY;
                 bearingTextObject.defaultText = "0.0";
                 bearingTextObject.maxLength = 5;
                 bearingTextObject.color = ColorRGB.WHITE;

commit f9ffc4248f6ff5712dbc477c797d478f3354d9db

Fixed text not restoring from saved state properly.  Broken with f38cb2c96cd4fa22f19e5b2abeddcd8f8d3ce040.

mccore/src/main/java/minecrafttransportsimulator/baseclasses/ComputedVariable.java
@@ -29,8 +29,8 @@ public class ComputedVariable {
     private ComputedVariableOperator function;
     private boolean changesOnPartialTicks;
     private boolean randomVariable;
-    public boolean isConstant;
-    public boolean shouldSaveToNBT;
+    private boolean isConstant;
+    private boolean shouldSaveToNBT;
     public boolean shouldReset = true;
     private long lastTickChecked;
     /**The current value of this variable.  Only change by calling one of the functions in this class.**/

mccore/src/main/java/minecrafttransportsimulator/entities/components/AEntityD_Definable.java
@@ -173,8 +173,8 @@ public abstract class AEntityD_Definable<JSONDefinition extends AJSONMultiModelP
             if (definition.rendering != null && definition.rendering.textObjects != null) {
                 for (int i = 0; i < definition.rendering.textObjects.size(); ++i) {
                     JSONText textDef = definition.rendering.textObjects.get(i);
-                    //Check for text value in case we added a text line after wr created this entity.
-                    text.put(textDef, data.hasKey("textLine" + i) ? data.getString("textLine" + i) : textDef.defaultText);
+                    //Check for text value in case we added a text line after we created this entity.
+                    text.put(textDef, data.hasKey("text" + textDef.fieldName) ? data.getString("text" + textDef.fieldName) : textDef.defaultText);
                 }
             }
 
@@ -1267,12 +1267,7 @@ public abstract class AEntityD_Definable<JSONDefinition extends AJSONMultiModelP
     public IWrapperNBT save(IWrapperNBT data) {
         super.save(data);
         data.setPackItem(definition, subDefinition.subName);
-        if (!text.isEmpty()) {
-            int lineNumber = 0;
-            for (String textLine : text.values()) {
-                data.setString("textLine" + lineNumber++, textLine);
-            }
-        }
+        text.entrySet().forEach(textEntry -> data.setString("text" + textEntry.getKey().fieldName, textEntry.getValue()));
         List<String> savedNames = new ArrayList<>();
         computedVariables.values().forEach(variable -> {
             if (variable.entity == this) {
@@ -1280,6 +1275,7 @@ public abstract class AEntityD_Definable<JSONDefinition extends AJSONMultiModelP
             }
         });
         if (!savedNames.isEmpty()) {
+            //Don't want to save variables if we don't have any set since it prevents stacking.
             data.setStrings("variables", savedNames);
         }
         return data;