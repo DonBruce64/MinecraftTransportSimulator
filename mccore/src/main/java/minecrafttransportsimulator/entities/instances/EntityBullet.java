f74d60343c1de7e0bbe390db8698b148763cc2d3
DonBruce64 <rapscallion827@gmail.com>
2025-01-12 19:24:38 -0600

    Fixed bullet explosions not hurting entites when they hit blocks.  Partially addresses #1561.

--- mccore/src/main/java/minecrafttransportsimulator/entities/instances/EntityBullet.java
+++ mccore/src/main/java/minecrafttransportsimulator/entities/instances/EntityBullet.java
@@ -637,7 +637,11 @@ public class EntityBullet extends AEntityD_Definable<JSONBullet> {
         //Spawn an explosion if we are an explosive bullet on the server.
         if (!gun.world.isClient() && ConfigSystem.settings.damage.bulletExplosions.value && gun.lastLoadedBullet.definition.bullet.types.contains(BulletType.EXPLOSIVE)) {
             float blastSize = gun.lastLoadedBullet.definition.bullet.blastStrength == 0 ? gun.lastLoadedBullet.definition.bullet.diameter / 10F : gun.lastLoadedBullet.definition.bullet.blastStrength;
-            gun.world.spawnExplosion(position, blastSize, gun.lastLoadedBullet.definition.bullet.types.contains(BulletType.INCENDIARY) && ConfigSystem.settings.damage.bulletBlockBreaking.value, ConfigSystem.settings.damage.bulletBlockBreaking.value);
+            Point3D explosionPosition = position.copy();
+            if (hitType == HitType.BLOCK) {
+                explosionPosition.add(hitSide.xOffset, hitSide.yOffset, hitSide.zOffset);
+            }
+            gun.world.spawnExplosion(explosionPosition, blastSize, gun.lastLoadedBullet.definition.bullet.types.contains(BulletType.INCENDIARY) && ConfigSystem.settings.damage.bulletBlockBreaking.value, ConfigSystem.settings.damage.bulletBlockBreaking.value);
         }
 
         EntityBullet bullet = gun.world.getBullet(gun.uniqueUUID, bulletNumber);
