fe56752dd90972f115c83148fd6db89c4395f467
DonBruce64 <rapscallion827@gmail.com>
2025-08-03 14:41:03 -0500

    Updated to 22.18.0.

--- mcinterfaceforge1201/src/main/java/mcinterface1201/InterfaceLoader.java
+++ mcinterfaceforge1201/src/main/java/mcinterface1201/InterfaceLoader.java
@@ -61,7 +61,7 @@ import net.minecraftforge.registries.DeferredRegister;
 public class InterfaceLoader {
     public static final String MODID = "mts";
     public static final String MODNAME = "Immersive Vehicles (MTS)";
-    public static final String MODVER = "22.17.0";
+    public static final String MODVER = "22.18.0";
 
     private final FMLJavaModLoadingContext context;
     public static final Logger LOGGER = LogManager.getLogger(InterfaceLoader.MODID);
