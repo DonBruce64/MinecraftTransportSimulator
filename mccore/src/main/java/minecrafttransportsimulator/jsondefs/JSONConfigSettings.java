738175a85d737d7f87468632d083abfc4beb7047
SnailPerson <97041048+SnailPerson@users.noreply.github.com>
2026-03-13 16:19:17 +0100

    Update JSONConfigSettings.java
    Set default external damage value to true, so people dont get confused by why they cant damage vehicles with their weapons

--- mccore/src/main/java/minecrafttransportsimulator/jsondefs/JSONConfigSettings.java
+++ mccore/src/main/java/minecrafttransportsimulator/jsondefs/JSONConfigSettings.java
@@ -92,7 +92,7 @@ public class JSONConfigSettings {
         public JSONConfigEntry<Boolean> creativePlayerDamage = new JSONConfigEntry<>(false, "If true, damage from vehicles and guns will be applied to creative players.  Not sure why you'd want this, but it's here.");
         public JSONConfigEntry<Boolean> wheelBreakage = new JSONConfigEntry<>(true, "Whether or not wheels can be broken (go flat).");
         public JSONConfigEntry<Boolean> wheelDamageIgnoreVelocity = new JSONConfigEntry<>(false, "Whether or not velocity is ignored when calculating wheel damage.");
-        public JSONConfigEntry<Boolean> allowExternalDamage = new JSONConfigEntry<>(false, "Whether or not non-IV things can damage vehicles.  This is normally false, as external damage is a hassle for most people, but can be true if you want other mod's guns to be able to attack vehicles in addition to IV's.");
+        public JSONConfigEntry<Boolean> allowExternalDamage = new JSONConfigEntry<>(true, "Whether or not non-IV things can damage vehicles.  This is normally false, as external damage is a hassle for most people, but can be true if you want other mod's guns to be able to attack vehicles in addition to IV's.");
         public JSONConfigEntry<Double> propellerDamageFactor = new JSONConfigEntry<>(1.0D, "Factor for damage caused by a propeller.");
         public JSONConfigEntry<Double> jetDamageFactor = new JSONConfigEntry<>(1.0D, "Factor for damage caused by a jet engine.");
         public JSONConfigEntry<Double> wheelDamageFactor = new JSONConfigEntry<>(1.0D, "Factor for damage caused by wheels on vehicles.");
@@ -196,6 +196,7 @@ public class JSONConfigSettings {
         NOTHING;
     }
 }
+
 
 
 
