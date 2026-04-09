df1dbc5212f48ae16b76596d5b593265c77db89b
dldev32 <vladvolkov100@mail.ru>
2026-04-09 18:26:09 +0300

    Added damage to entities from collisions with vehicle collisions

--- mccore/src/main/java/minecrafttransportsimulator/jsondefs/JSONConfigSettings.java
+++ mccore/src/main/java/minecrafttransportsimulator/jsondefs/JSONConfigSettings.java
@@ -99,6 +99,8 @@ public class JSONConfigSettings {
         public JSONConfigEntry<Double> crashDamageFactor = new JSONConfigEntry<>(1.0D, "Factor for damage caused by crashes.");
         public JSONConfigEntry<Double> bulletDamageFactor = new JSONConfigEntry<>(1.0D, "Factor for damage caused by bullets on vehicles.");
         public JSONConfigEntry<Double> wheelDamageMinimumVelocity = new JSONConfigEntry<>(0.2D, "Minimum velocity (blocks/second) which vehicles must be going to damage entities with their wheels.");
+        public JSONConfigEntry<Double> collisionDamageFactor = new JSONConfigEntry<>(1.0D, "Factor for damage caused by vehicle's collision");
+        public JSONConfigEntry<Double> collisionDamageMinimumVelocity = new JSONConfigEntry<>(0.2D, "Minimum velocity (blocks/second) which vehicles must be going to damage entities when collide with entities.");
         public JSONConfigEntry<Map<String, Double>> packBulletDamageFactors = new JSONConfigEntry<>(new HashMap<>(), "A mapping of pack-speciifc bullet damage factors.  These values will apply to all bullets in a pack when they hit something, be it a vehicle or entity.");
     }
 
